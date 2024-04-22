cf7ee7cbf1049642a439c175f32e61328ea16259
Arghya Ghosh <uiuxarghya@gmail.com>
2024-04-22 14:57:00 +0530

    feat: update binomialCoefficient.c

--- Dynamic Programming/binomialCoefficient.c
+++ Dynamic Programming/binomialCoefficient.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 
 int binarySearch(int arr[], int x, int low, int high) {
-
   while (low <= high) {
     int mid = low + (high - low) / 2;
 
-    if (arr[mid] == x)
+    if (arr[mid] == x)  // element found at mid
       return mid;
 
-    if (arr[mid] < x)
+    if (arr[mid] < x)  // element is greater
       low = mid + 1;
-
-    else
+    else  // element is smaller
       high = mid - 1;
   }
 
-  return -1;
+  return -1;  // element not found
 }
 
 int main(void) {
@@ -24,7 +22,7 @@ int main(void) {
   printf("Enter your array length: ");
   scanf("%d", &n);
 
-  int arr[n];
+  int arr[n];  // declare array
 
   printf("\nEnter values in ascending order\n");
   for (i = 0; i < n; i++) {
@@ -35,7 +33,7 @@ int main(void) {
   printf("\nEnter the element to search: ");
   scanf("%d", &num);
 
-  int result = binarySearch(arr, num, 0, n - 1);
+  int result = binarySearch(arr, num, 0, n - 1);  // store result
 
   if (result == -1)
     printf("Element not found!\n");
