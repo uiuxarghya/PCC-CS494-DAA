74f4256e7900909d6276f4ee66bd16fa3d7c67f5
Arghya Ghosh <uiuxarghya@gmail.com>
2024-04-27 23:44:23 +0530

    feat: update bellmanFord.c

--- Graph/Shortest Path (DP)/bellmanFord.c
+++ Graph/Shortest Path (DP)/bellmanFord.c
@@ -3,24 +3,27 @@
 
 int vertices, edges;
 
+// function to perform the bellman-ford algorithm
 void bellmanFord(int graph[edges][3], int edges, int distance[], int source)
 {
-    distance[source] = 0;
+    distance[source] = 0; // set the source distance to 0
     int count;
-    for (count = 1; count < vertices; count++)
+    for (count = 1; count < vertices; count++) // loop to relax all edges (vertices-1) times
     {
         int i, u, v, wt;
         for (i = 0; i < edges; i++)
         {
-            u = graph[i][0];
-            v = graph[i][1];
-            wt = graph[i][2];
+            u = graph[i][0]; // start vertex of the edge
+            v = graph[i][1]; // end vertex of the edge
+            wt = graph[i][2]; // weight of the edge
+            // relax the edge if possible
             if (distance[u] != INT_MAX && (distance[u] + wt) < distance[v])
                 distance[v] = distance[u] + wt;
         }
     }
 }
 
+// function to print the shortest distances from the source to all other vertices
 void printDistance(int distance[], int source)
 {
     int i;
@@ -28,29 +31,29 @@ void printDistance(int distance[], int source)
     printf("Vertex\tDistance\n");
     for (i = 0; i < vertices; i++)
     {
-        if (i == source)
+        if (i == source) // skip printing the source itself
             continue;
-        printf("%d     \t%d\n", i, distance[i]);
+        printf("%d     \t%d\n", i, distance[i]); // print the vertex and its distance from the source
     }
 }
 
 int main()
 {
     printf("Enter the no. of Vertices : ");
-    scanf("%d", &vertices);
+    scanf("%d", &vertices); // input number of vertices
     int distance[vertices];
     int source, i;
     printf("Enter no. of Edges : ");
-    scanf("%d", &edges);
+    scanf("%d", &edges); // input number of edges
     int graph[edges][3];
     printf("Enter the Adjacency List (Source, Destination and Cost) : \n");
-    for (i = 0; i < edges; i++)
+    for (i = 0; i < edges; i++) // input each edge including its source, destination, and weight
         scanf("%d %d %d", &graph[i][0], &graph[i][1], &graph[i][2]);
     for (i = 0; i < vertices; i++)
-        distance[i] = INT_MAX;
+        distance[i] = INT_MAX; // initialize distances to infinity
     printf("Enter the Source vertex : ");
-    scanf("%d", &source);
-    bellmanFord(graph, edges, distance, source);
-    printDistance(distance, source);
+    scanf("%d", &source); // input the source vertex
+    bellmanFord(graph, edges, distance, source); // run the bellman-ford algorithm
+    printDistance(distance, source); // print the results
     return 0;
 }
